2ea600ac817ccc84448af8878e49c0b9aae4fc6c
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2022-07-29 14:28:52 +0300

    Added property in ApplicationDbContext

--- ContosoUniversity.Data/ApplicationDbContext.cs
+++ ContosoUniversity.Data/ApplicationDbContext.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ContosoUniversity.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
             : base(options)
         {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
         }
     }
 }
